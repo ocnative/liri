9007b02d88ab4a6a598cecff8c5cedccfd60a8a3
Bill <lrpc949@gmail.com>
2017-08-16 19:32:54 -0700

    First commit.

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'PIKhCVTvSGMhxMz9EXKt4vDwh',
+  consumer_secret: 'jFoXGkiS795MT8iauygjCruf2oi2OVpWkmxwfuGmV6QpmH33N4',
+  access_token_key: '897974113049862145-ADvScJkQT6V512FztTEh53UbUqLfm7W',
+  access_token_secret: '1ItYzKdmCy1UE6qFF0fZgJ5h1Rkt1rZGKZZKdGy2bbvx6',
+}
+
